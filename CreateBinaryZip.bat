5f7c244598203ce62cddacb11dffc3ecfc4b4101
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2019-08-26 16:35:47 +1000

    Bumped the version to 8.0.18

--- CreateBinaryZip.bat
+++ CreateBinaryZip.bat
@@ -112,7 +112,7 @@ ECHO "| <version>   must be a 3 number version              |"
 ECHO "|                                                     |"
 ECHO "| EXAMPLE                                             |"
 ECHO "|                                                     |"
-ECHO "| CreateBinaryZip 8.0.17                              |"
+ECHO "| CreateBinaryZip 8.0.18                              |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
 
