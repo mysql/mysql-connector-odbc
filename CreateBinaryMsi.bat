de250b6195283d34ed4b291dcf906ea9106c9e64
Rafal Somla <rafal.somla@oracle.com>
2020-04-01 11:02:24 +0200

    Bumped the version to 8.0.21

--- CreateBinaryMsi.bat
+++ CreateBinaryMsi.bat
@@ -108,7 +108,7 @@ ECHO "|              i - internal                           |"
 ECHO "|                                                     |"
 ECHO "| EXAMPLE                                             |"
 ECHO "|                                                     |"
-ECHO "| CreateBinaryMsi 8.0.20                              |"
+ECHO "| CreateBinaryMsi 8.0.21                              |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
 
