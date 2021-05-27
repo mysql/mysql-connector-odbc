f28e093ff9879b44733fcd1b377dc3cdf9cb5e30
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2021-05-27 17:08:08 +1000

    Bug #32925338 MSQRY32 + ODBC REPORTS: 'THIS DATA SOURCE CONTAINS NO VISIBLE TABLES' FIRST TIME

--- driver/connect.cc
+++ driver/connect.cc
@@ -296,6 +296,10 @@ SQLRETURN DBC::connect(DataSource *dsrc)
   if (GetModuleHandle("sqlservr.exe") != NULL)
     dsrc->pad_char_to_full_length= 1;
 
+  /* Detect MS Query and prevent if from using schema as database */
+  if (GetModuleHandle("msqry32.exe") != NULL)
+    dsrc->no_schema = 1;
+
 #endif
 
   mysql = mysql_init(nullptr);
