89e2ca91b0b8325c9ce025a563e490903230f666
Luís Silva <luis.m.silva@oracle.com>
2021-11-26 12:26:26 +0000

    Use cmake3 on el6 and 3l7. Update minimum cmake version to 3.2

--- scripts/myodbc3.spec.sh
+++ scripts/myodbc3.spec.sh
@@ -1,30 +1,30 @@
 # Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
-# 
-# This program is free software; you can redistribute it and/or modify 
-# it under the terms of the GNU General Public License, version 2.0, as 
-# published by the Free Software Foundation. 
 #
-# This program is also distributed with certain software (including 
-# but not limited to OpenSSL) that is licensed under separate terms, 
-# as designated in a particular file or component or in included license 
-# documentation. The authors of MySQL hereby grant you an 
-# additional permission to link the program and your derivative works 
-# with the separately licensed software that they have included with 
-# MySQL. 
-# 
-# Without limiting anything contained in the foregoing, this file, 
-# which is part of MySQL Connector/ODBC, is also subject to the 
-# Universal FOSS Exception, version 1.0, a copy of which can be found at 
-# http://oss.oracle.com/licenses/universal-foss-exception. 
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-# See the GNU General Public License, version 2.0, for more details. 
-# 
-# You should have received a copy of the GNU General Public License 
-# along with this program; if not, write to the Free Software Foundation, Inc., 
-# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0, as
+# published by the Free Software Foundation.
+#
+# This program is also distributed with certain software (including
+# but not limited to OpenSSL) that is licensed under separate terms,
+# as designated in a particular file or component or in included license
+# documentation. The authors of MySQL hereby grant you an
+# additional permission to link the program and your derivative works
+# with the separately licensed software that they have included with
+# MySQL.
+#
+# Without limiting anything contained in the foregoing, this file,
+# which is part of MySQL Connector/ODBC, is also subject to the
+# Universal FOSS Exception, version 1.0, a copy of which can be found at
+# http://oss.oracle.com/licenses/universal-foss-exception.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 
 ##############################################################################
@@ -43,6 +43,8 @@
 %global license_type	GPLv2
 %global license_files	LICENSE.txt README.txt
 %endif
+
+%global cmake3  cmake%{?el6:3}%{?el7:3}
 
 # Use rpmbuild -ba --define 'shared_mysqlclient 1' ... to build shared
 %{!?shared_mysqlclient: %global static_mysqlclient 1}
@@ -133,7 +135,7 @@ The test suite for MySQL ODBC.
 mkdir release
 pushd release
 export CFLAGS="%{optflags}"
-cmake -G "Unix Makefiles"   \
+%{cmake3} -G "Unix Makefiles"   \
     -DRPM_BUILD=1           \
     -DCMAKE_INSTALL_PREFIX=%{_prefix} \
 %if 0%{?odbc_gui}
@@ -146,7 +148,7 @@ cmake -G "Unix Makefiles"   \
     %{?cmake_opt_extra}      \
     ..
 
-# Note that the ".." needs to be last, in case some arguments expands to 
+# Note that the ".." needs to be last, in case some arguments expands to
 # the empty string, and then "cmake" thinks is "current directory"
 
 make  %{?_smp_mflags} VERBOSE=1
@@ -188,7 +190,7 @@ popd
 # deinstall before reinstall to change anything
 # ----------------------------------------------------------------------
 
-%post 
+%post
 if [ -x /usr/bin/myodbc-installer ]; then
     /usr/bin/myodbc-installer -a -d -n "MySQL ODBC @CONNECTOR_BASE_VERSION@ Unicode Driver" -t "DRIVER=%{_libdir}/libmyodbc8w.so"
     /usr/bin/myodbc-installer -a -d -n "MySQL ODBC @CONNECTOR_BASE_VERSION@ ANSI Driver"    -t "DRIVER=%{_libdir}/libmyodbc8a.so"
@@ -203,7 +205,7 @@ fi
 %endif
 
 # ----------------------------------------------------------------------
-# DEREGISTER DRIVER 
+# DEREGISTER DRIVER
 # ----------------------------------------------------------------------
 
 # Removing the driver package, we simply orphan any related DSNs
