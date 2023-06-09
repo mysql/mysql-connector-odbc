5f873dc8e5b9add163809b281d7f9d8ac6baa2ce
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2023-06-09 16:13:55 +1000

    Fix OTel and OpenSSL dependencies
    Change-Id: I1ab920876dda3054494d232a0d0a7d4012fff091

--- cmake/libutils.cmake
+++ cmake/libutils.cmake
@@ -332,6 +332,8 @@ function(mangle_osx_rpaths TARGET)
   list(APPEND paths_to_mangle "libcrypto.1.0.0.dylib")
   list(APPEND paths_to_mangle "libssl.1.1.dylib")
   list(APPEND paths_to_mangle "libcrypto.1.1.dylib")
+  list(APPEND paths_to_mangle "libssl.3.dylib")
+  list(APPEND paths_to_mangle "libcrypto.3.dylib")
 
   set(mangle_commands)
 
