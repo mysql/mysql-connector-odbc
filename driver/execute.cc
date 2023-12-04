866ad04ca851d67e1c8e9d68aa4b8f57ecfdd1a9
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2023-12-04 21:53:34 +1100

    Check the server capabilities instead of the version for the attributes support
    Change-Id: I2ed95b4dfdb9fbd6bf17ae24dd2c6302b30f170a

--- driver/execute.cc
+++ driver/execute.cc
@@ -125,7 +125,8 @@ SQLRETURN do_query(STMT *stmt, std::string query)
 #if MYSQL_VERSION_ID >= 80300
       // For older servers that don't support named params
       // we just don't count them and specify the number of unnamed params.
-      unsigned int p_number = is_minimum_version(stmt->dbc->mysql->server_version, "8.3.0") ?
+      unsigned int p_number =
+        stmt->dbc->mysql->server_capabilities & CLIENT_QUERY_ATTRIBUTES ?
         stmt->query_attr_names.size() : stmt->param_count;
 
       if (p_number)
