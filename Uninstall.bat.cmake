78eb23b6c0566f96c80e780351f9619d7f3799fd
Rafał Somla <rafal.somla@oracle.com>
2024-03-01 16:27:41 +0100

    GPL License Exception Update

--- Uninstall.bat.cmake
+++ Uninstall.bat.cmake
@@ -1,31 +1,31 @@
 @ECHO OFF
-REM Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved. 
-REM 
-REM This program is free software; you can redistribute it and/or modify 
-REM it under the terms of the GNU General Public License, version 2.0, as 
-REM published by the Free Software Foundation. 
+REM Copyright (c) 2006, 2024, Oracle and/or its affiliates.
 REM
-REM This program is also distributed with certain software (including 
-REM but not limited to OpenSSL) that is licensed under separate terms, 
-REM as designated in a particular file or component or in included license 
-REM documentation. The authors of MySQL hereby grant you an 
-REM additional permission to link the program and your derivative works 
-REM with the separately licensed software that they have included with 
-REM MySQL. 
-REM 
-REM Without limiting anything contained in the foregoing, this file, 
-REM which is part of MySQL Server, is also subject to the 
-REM Universal FOSS Exception, version 1.0, a copy of which can be found at 
-REM http://oss.oracle.com/licenses/universal-foss-exception. 
-REM 
-REM This program is distributed in the hope that it will be useful, but 
-REM WITHOUT ANY WARRANTY; without even the implied warranty of 
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-REM See the GNU General Public License, version 2.0, for more details. 
-REM 
-REM You should have received a copy of the GNU General Public License 
-REM along with this program; if not, write to the Free Software Foundation, Inc., 
-REM 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+REM This program is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License, version 2.0, as
+REM published by the Free Software Foundation.
+REM
+REM This program is designed to work with certain software (including
+REM but not limited to OpenSSL) that is licensed under separate terms, as
+REM designated in a particular file or component or in included license
+REM documentation. The authors of MySQL hereby grant you an additional
+REM permission to link the program and your derivative works with the
+REM separately licensed software that they have either included with
+REM the program or referenced in the documentation.
+REM
+REM Without limiting anything contained in the foregoing, this file,
+REM which is part of Connector/ODBC, is also subject to the
+REM Universal FOSS Exception, version 1.0, a copy of which can be found at
+REM https://oss.oracle.com/licenses/universal-foss-exception.
+REM
+REM This program is distributed in the hope that it will be useful, but
+REM WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM See the GNU General Public License, version 2.0, for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program; if not, write to the Free Software Foundation, Inc.,
+REM 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 REM #########################################################
 REM
